d87eea42d4f9f1be3a83cc57ec51d631778409ce
Jordan Trask <jtrask@gmail.com>
2020-01-23 21:55:09 -0800

    Updated mtime

--- aliases.zsh
+++ aliases.zsh
@@ -20,7 +20,8 @@ alias fdcount="tree | grep directories"
 alias whatsmyip='dig @resolver1.opendns.com A myip.opendns.com +short -4;dig @resolver1.opendns.com AAAA myip.opendns.com +short -6'
 
 # - Shell Aliases
-alias mtime="find . -type f -printf "\n%TD %AT %p" | sort -k1.8n -k1.1nr -k1 | less"
+#alias mtime="find . -type f -printf "\n%TD %TT %p" | sort -k1.8n -k1.1nr -k1 | less"
+alias mtime="find . -type f -printf "%TY-%Tm-%Td %TT %p\n" | sort -r | less"
 
 # -- Web Aliases
 alias ttfb='curl -s -o /dev/null -w "Connect: %{time_connect} TTFB: %{time_starttransfer} Total time: %{time_total} \n" $1'
