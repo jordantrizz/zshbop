2b40bff9cf34248dba401d864289601e0f1f3303
Jordan Trask <jtrask@gmail.com>
2020-02-07 13:14:05 -0800

    Fixing some pathing

--- functions.zsh
+++ functions.zsh
@@ -118,6 +118,8 @@ init_ultb () {
         if [[ -a $ZSH_ROOT/ultimate-linux-tool-box/.zshrc ]]; then
                 _echo "-- Including Ultimate Linux Tool Box Paths"
                 source $ZSH_ROOT/ultimate-linux-tool-box/.zshrc
+        elfi
+        	PATH=$PATH:$ZSH_ROOT/ultimate-linux-tool-box
         fi
 }
 
@@ -126,7 +128,9 @@ init_uwt () {
         if [[ -a $ZSH_ROOT/ultimate-wordpress-tools/.zshrc ]]; then
                 _echo "-- Including Ultimate WordPress Tools"
                 source $ZSH_ROOT/ultimate-wordpress-tools/.zshrc
-        fi
+        elif
+	PATH=$PATH:$ZSH_ROOT/ultimate-wordpress-tools
+	fi
 }
 
 ####-- Clear Cache
