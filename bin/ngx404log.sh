69955db7df9719860fedf9deedc4b878f4bfc252
Jordan Trask <jtrask@gmail.com>
2021-11-01 10:48:35 -0400

    Updated nginx-log-404

--- bin/ngx404log.sh
+++ bin/ngx404log.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env zsh
-nginx-log-404 () {
-        local CAT
-        local EGREP
-        local FULLCMD
-        if [ -z $1 ]; then
-                echo "./$0 <logfile> <type> <strip>"
-                echo ""
-                echo "All arguments are required execpt for <strip>"
-                echo ""
-                echo "  <logfile> is a nginx log file."
-                echo "  <type> (n)ginx or (g)ridpane"
-                echo "  <strip> is a list of file excludes"
-                echo ""
-                echo "  example: ./$0 access_log n '.js|.css'"
+local CAT
+local EGREP
+local FULLCMD
+if [ -z $1 ] || [ -z $2 ]; then
+	echo "./$0 <logfile> <type> <strip>"
+        echo ""
+        echo "All arguments are required execpt for <strip>"
+        echo ""
+        echo "  <logfile> is a nginx log file."
+        echo "  <type> (n)ginx or (g)ridpane"
+        echo "  <strip> is a list of file excludes"
+        echo ""
+        echo "  example: ./$0 access_log n '.js|.css'"
+else
+	if [[ $1 == *.gz ]]; then CAT="zcat"; else CAT="cat"; fi
+        if [ ! -z $3 ]; then
+        	FULLCMD="$CAT $1 | egrep -v '$3'"
         else
-                if [[ $1 == *.gz ]]; then CAT="zcat"; else CAT="cat"; fi
-                if [ ! -z $3 ]; then
-                        FULLCMD="$CAT $1 | egrep -v '$3'"
-                else
-                        FULLCMD="$CAT $1"
-                fi
-                if [ $3 == "n" ]; then
-                        eval $FULLCMD | awk '($8 ~ /404/)' | awk '{print $8}' | sort | uniq -c | sort -rn
-                elif [ $3 == "g"]; then
-                        eval $FULLCMD | awk '($10 ~ /404/)' | awk '{print $8}' | sort | uniq -c | sort -rn
-                fi
+        	FULLCMD="$CAT $1"
         fi
-}
+        if [[ $2 -eq "n" ]]; then
+		echo " -- Running nginx 404"
+        	eval $FULLCMD | awk '($8 ~ /404/)' | awk '{print $8}' | sort | uniq -c | sort -rn
+        elif [[ $2 -eq "g" ]]; then
+        	echo " -- Running gridpane 404"
+        	eval $FULLCMD | awk '($10 ~ /404/)' | awk '{print $8}' | sort | uniq -c | sort -rn
+        fi
+fi
