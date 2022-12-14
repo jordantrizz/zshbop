2b7687ca65fcbdd4e03450fd5b19bddf31a72b53
Jordan Trask <jtrask@gmail.com>
2022-12-13 22:57:30 -0500

    Updated

--- cmds/cmds-mysql.zsh
+++ cmds/cmds-mysql.zsh
@@ -81,7 +81,7 @@ mysql-currentmem () {
 		MYSQL_CMD="mysql"
 	fi
 	
-	TMP_TABLE_SIZE=$(${MYSQL_CMD} --skip-column-names --silent --raw -e 'select TRIM(LEADING '0' from @@global.tmp_table_size)/1024/1024')
+	TMP_TABLE_SIZE=$(eval ${MYSQL_CMD} --skip-column-names --silent --raw -e 'select TRIM(LEADING '0' from @@global.tmp_table_size)/1024/1024')
 	KEY_BUFFER_SIZE=$(${MYSQL_CMD} --skip-column-names --silent --raw -e 'select TRIM(LEADING '0' from@@global.key_buffer_size)/1024/1024')
 	INNODB_BUFFER_POOL_SIZE=$(${MYSQL_CMD} --skip-column-names --silent --raw -e 'select TRIM(LEADING '0' from @@global.innodb_buffer_pool_size)/1024/1024')
 	INNODB_LOG_BUFFER_SIZE=$(${MYSQL_CMD} --skip-column-names --silent --raw -e 'select TRIM(LEADING '0' from @@global.innodb_log_buffer_size)/1024/1024')
