91538c750225ac1a34b1714a5cae4054863fe3b8
Jordan Trask <jtrask@gmail.com>
2023-01-04 11:07:01 -0500

    Updated

--- functions.zsh
+++ functions.zsh
@@ -92,7 +92,7 @@ _echo () { echo "$@" }
 _error () { echo  "$fg[red] * $@ $reset_color" }
 _warning () { echo "$fg[yellow] * $@ $reset_color" }
 _success () { echo "$fg[green] * $@ $reset_color" }
-_notice () { echo "$fg[magenta] * $@ $reset_color" }
+_notice () { echo "$bg[magenta]$fg[white] * $@ $reset_color" }
 
 # -- Banners
 _banner_red () { echo "$bg[red]$fg[white] $@ $reset_color" }
