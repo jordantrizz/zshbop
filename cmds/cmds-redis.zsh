9d12751697a6808b82a30a49744e9a0c41314350
Jordan Trask <jtrask@gmail.com>
2023-01-04 11:14:39 -0500

    Added cmds-redis.zsh

---
+++ cmds/cmds-redis.zsh
@@ -0,0 +1,27 @@
+# --
+# template commands
+#
+# Example help: help_template[test]='Generate phpinfo() file'
+#
+# --
+_debug " -- Loading ${(%):-%N}"
+
+# What help file is this?
+help_files[redis_description]="Redis commands"
+help_files[redis]="Redis Commands"
+
+# - Init help array
+typeset -gA help_redis
+
+_debug " -- Loading ${(%):-%N}"
+
+redis-memory () {
+	if [[ -f /etc/redis/redis.conf ]]; then
+		_notice "Redis 'maxmemory' setting from /etc/redis/redis.conf"
+		grep 'maxmemory' /etc/redis/redis.conf
+	else
+		_error "No /etc/redis/redis.conf file"
+	fi
+	_notice "Getting redis-cli info memory"
+	redis-cli info memory
+}
