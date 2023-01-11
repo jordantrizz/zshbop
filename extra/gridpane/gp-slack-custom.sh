e09e6a8b7b34b34c56d27faa7c637fb546c70ab3
Jordan Trask <jtrask@gmail.com>
2023-01-11 11:51:48 -0500

    Updated

--- extra/gridpane/gp-slack-custom.sh
+++ extra/gridpane/gp-slack-custom.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# - source GridPane
 source "/usr/local/bin/lib/gridpane.sh"
 gridpane::setglobals
 
+# - configure message
 slack_type="error"
 title="$1"
 error_message="$2"
 slack_details="Server Name: ${host}{{newline}}Server IP: ${serverIP}{{newline}} $error_message"
 echo $slack_details
 event_type="sys_load_avg"
+
+# - send slack
 preemptive_support="false"
       gridpane::notify::slack \
         "${slack_type}" \
