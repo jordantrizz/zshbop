a613a54a971c0173091950261259345b2f637328
Jordan Trask <jtrask@gmail.com>
2023-01-12 02:51:10 +0000

    Updated

--- extra/gridpane/gp-slack-custom.sh
+++ extra/gridpane/gp-slack-custom.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 #
-# Usage: gp-slack-custom.sh 'title' 'error_message'
+# Usage: gp-slack-custom.sh <slack message title>
 #
 
 # - source GridPane
-source "/usr/local/bin/lib/gridpane.sh"
-gridpane::setglobals
+source "/usr/local/bin/lib/gridpane.sh" # - Contains lots of GridPane functions
+gridpane::setglobals # - Needed for gridpane::notify:slack
 
-# - configure message
-slack_type="error"
-title="$1"
-error_message="$2"
-slack_details="Server Name: ${host}{{newline}}Server IP: ${serverIP}{{newline}} $error_message"
-event_type="sys_load_avg"
+# - Slack message
+slack_type="error" # can be warning or error and maybe success?
+title="$1" # slack title
+slack_details="Server Name: ${host}{{newline}}Server IP: ${serverIP}{{newline}} $MONIT_EVENT" # full details for slack message
+event_type="sys_load_avg" # Used because the API won't accept anything but specific event_types
 
 # - send slack
 preemptive_support="false"
