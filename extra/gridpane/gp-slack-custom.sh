8c50c01727632ebe28468589838f6d5f824aae56
Jordan Trask <jtrask@gmail.com>
2023-01-12 05:17:12 -0500

    Updated

--- extra/gridpane/gp-slack-custom.sh
+++ extra/gridpane/gp-slack-custom.sh
@@ -7,8 +7,7 @@ gridpane::setglobals
 # - configure message
 slack_type="error"
 title="$1"
-error_message="$2"
-slack_details="Server Name: ${host}{{newline}}Server IP: ${serverIP}{{newline}} $error_message"
+slack_details="Server Name: ${host}{{newline}}Server IP: ${serverIP}{{newline}} $MONIT_EVENT - $MONIT_DESCRIPTION"
 echo $slack_details
 event_type="sys_load_avg"
 
