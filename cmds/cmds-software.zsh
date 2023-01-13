b354116ca1f1cdbccb34cc3a813f6389d289c0f3
Jordan Trask <jtrask@gmail.com>
2023-01-13 11:53:31 -0500

    Updated

--- cmds/cmds-software.zsh
+++ cmds/cmds-software.zsh
@@ -272,3 +272,12 @@ software_maldet () {
 	cd $TMP/maldetect-current
 	./install.sh
 }
+
+# -- software_speedtest-cli
+help_software[speedtest-cli]="Speedtest-cli from https://github.com/sivel/speedtest-cli"
+speedtest-cli () {
+	apt-get install python-pip git
+	pip install speedtest-cli	
+}
+
+apt-get install python-pip git
