f4f8a01490fe2fc9b489851329d4bd0be2703943
Jordan Trask <jtrask@gmail.com>
2023-01-23 06:44:30 -0500

    Fixed issues with check_diskspace and zshbop_cacheclear

--- init.zsh
+++ init.zsh
@@ -434,8 +434,11 @@ system_check () {
 	fi
 	
 	# -- check disk space
-	_loading2 "Checking disk space"
+	_loading2 "Checking disk space on $MACHINE_OS"
 	check_diskspace
+	echo $MACHINE_OS
+
+	# -- check block devices
 	_loading2 "Checking block devices"
 	check_blockdevices
 }	
