654eaef55fdf4188d226175b2f3f20e65dc78407
Jordan Trask <jtrask@gmail.com>
2023-03-16 21:10:59 -0400

    Updated

--- cmds/cmds-mysql.zsh
+++ cmds/cmds-mysql.zsh
@@ -55,13 +55,7 @@ mysql-msds () {
 # - mysql-myisam
 help_mysql[mysql-myisam]='Locate myisam tables in MySQL'
 mysql-myisam () { 
-	MYISAM=$(mysql -e "select table_schema,table_name,engine,table_collation from information_schema.tables where engine='MyISAM';")
-	if [[ -z ${MYISAM} ]]; then
-		echo ${MYISAM}
-		_error "Found MyISAM tables"
-	else
-		_success "No MyISAM tables found"
-	fi
+	mysql -e "select table_schema,table_name,engine,table_collation from information_schema.tables where engine='MyISAM';"
 }
 
 # - mysql-maxmem
