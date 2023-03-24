ec714c33347ddd1c80881c5df0b714306e48d5dc
Jordan Trask <jtrask@gmail.com>
2023-03-24 11:10:08 -0400

    Updated

--- cmds/cmds-software.zsh
+++ cmds/cmds-software.zsh
@@ -336,7 +336,7 @@ function software_jiq () {
 # -- plik-conf
 help_software[plik-conf]='Print out .plikrc'
 function plik-conf () {    
-    if [[ -f $HOME/.plikrc ]]; then
+    if [[ ! -f $HOME/.plikrc ]]; then
         _error "No $HOME/.plikrc exists"
         return 1
     else
