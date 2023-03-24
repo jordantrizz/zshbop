85c82a1ddbaaa385e7f3bcfacaa8ac9d337219cb
Jordan Trask <jtrask@gmail.com>
2023-03-24 11:32:58 -0400

    Added command mail-smtptest

--- cmds/cmds-mail.zsh
+++ cmds/cmds-mail.zsh
@@ -19,3 +19,50 @@ eximcq () { exim -bp | exiqgrep -i | xargs exim -Mrm }
 # -- postmark
 help_mail[postmark]='Postmark cli for sending email'
 alias postmark="postmark.sh"
+
+# -- mail-smtptest
+help_mail[mail-smtptest]='Test SMTP Login'
+function mail-smtptest () {
+    local hostname="${1:-smtp.example.com}"
+    local port="${2:-587}"
+    local username="${3:-user@example.com}"
+    local password="${4:-password}"
+    local response
+
+    # Make SMTP connection
+    echo "Trying to connect to $hostname on port $port..."
+    response="$(echo -ne "EHLO example.com\r\n" | nc -w 5 "$hostname" "$port")"
+    echo "Response: $response"
+
+    # Send STARTTLS command and initiate TLS handshake
+    echo "Sending STARTTLS command..."
+    response="$(echo -ne "STARTTLS\r\n" | nc -w 5 "$hostname" "$port")"
+    echo "Response: $response"
+
+    echo "Initiating TLS handshake..."
+    response="$(openssl s_client -quiet -connect "$hostname":"$port" < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > smtp.crt)"
+    echo "Response: $response"
+
+    # Send EHLO command again after TLS handshake
+    echo "Sending EHLO command again after TLS handshake..."
+    response="$(echo -ne "EHLO example.com\r\n" | nc -w 5 "$hostname" "$port")"
+    echo "Response: $response"
+
+    # Send AUTH LOGIN command and send encoded username and password
+    echo "Sending AUTH LOGIN command..."
+    response="$(echo -ne "AUTH LOGIN\r\n" | nc -w 5 "$hostname" "$port")"
+    echo "Response: $response"
+
+    echo "Sending Base64-encoded username..."
+    response="$(echo -ne "$username" | base64 | tr -d '\n' | sed 's/^/USER /' | sed 's/$/\r\n/' | nc -w 5 "$hostname" "$port")"
+    echo "Response: $response"
+
+    echo "Sending Base64-encoded password..."
+    response="$(echo -ne "$password" | base64 | tr -d '\n' | sed 's/^/PASS /' | sed 's/$/\r\n/' | nc -w 5 "$hostname" "$port")"
+    echo "Response: $response"
+
+    # Close connection
+    echo "Closing connection..."
+    response="$(echo -ne "QUIT\r\n" | nc -w 5 "$hostname" "$port")"
+    echo "Response: $response"
+}
