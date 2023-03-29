6fbd051cb987e1a58960250d02311f511a45bbb4
Jordan Trask <jtrask@gmail.com>
2023-03-29 10:36:59 -0400

    * Moving files around to have a cleaner root * Updated install.sh * Changed README.md to include image. * Fixed issue with installer.

--- zshbop.zsh
+++ zshbop.zsh
@@ -81,13 +81,13 @@ EDITOR_RUN=${${$(alias $EDITOR)#joe=\'}%\'}
 # ---------------
 # -- Source files
 # ---------------
-source ${ZSHBOP_ROOT}/colors.zsh # -- colors first!
-source ${ZSHBOP_ROOT}/functions-core.zsh #--
-source ${ZSHBOP_ROOT}/functions.zsh # -- 
-source ${ZSHBOP_ROOT}/init.zsh # -- include init
-source ${ZSHBOP_ROOT}/aliases.zsh # -- include functions
-source ${ZSHBOP_ROOT}/help.zsh # -- include help functions
-source ${ZSHBOP_ROOT}/kb.zsh # -- Built in Knolwedge Base
+source ${ZSHBOP_ROOT}/lib/colors.zsh # -- colors first!
+source ${ZSHBOP_ROOT}/lib/functions-core.zsh #--
+source ${ZSHBOP_ROOT}/lib/functions.zsh # -- 
+source ${ZSHBOP_ROOT}/lib/init.zsh # -- include init
+source ${ZSHBOP_ROOT}/lib/aliases.zsh # -- include functions
+source ${ZSHBOP_ROOT}/help/help.zsh # -- include help functions
+source ${ZSHBOP_ROOT}/kb/kb.zsh # -- Built in Knolwedge Base
 
 ############################################################
 
