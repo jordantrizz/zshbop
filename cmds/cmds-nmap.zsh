8024ad83197a85d98c0f4d4c128f0c0ecb4d50e5
Jordan Trask <jtrask@gmail.com>
2023-04-28 10:43:32 -0400

    .

--- cmds/cmds-nmap.zsh
+++ cmds/cmds-nmap.zsh
@@ -20,7 +20,7 @@ _debug " -- Loading ${(%):-%N}"
 # -- ping-sweep
 help_nmap[ping-sweep]="Ping sweep, return on line hosts with ARP and vendor ID's"
 ping-sweep () {
-	if [[ -n $1 ]]; then
+	if [[ -z $1 ]]; then
 			echo "Usage: ping-sweep <range>"
 			echo "	Example: ping-sweep 192.168.1.1-255"
 	else
@@ -31,7 +31,7 @@ ping-sweep () {
 # -- open-ports
 help_nmap[open-ports]="Scan open ports -F (Fast)"
 open-ports () {
-		if [[ -n $1 ]]; then
+		if [[ -z $1 ]]; then
 			echo "Usage: open-ports <range>"
 			echo "	Example: open-ports 192.168.1.1"
 		else
