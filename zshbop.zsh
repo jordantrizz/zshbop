2d96721b5de357f8a6d34418d6a3b2423956daf3
Jordan Trask <jtrask@gmail.com>
2023-05-01 14:55:15 +0000

    Added setopt for OMZ history plugin to persist history across open zsh shells

--- zshbop.zsh
+++ zshbop.zsh
@@ -77,6 +77,14 @@ EDITOR_RUN=${${$(alias $EDITOR)#joe=\'}%\'}
 ####-- diff-so-fancy
 # git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 
+# -- OMZ History Plugin
+setopt extended_history       # record timestamp of command in HISTFILE
+setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_verify            # show command with history expansion to user before running it
+setopt share_history          # share command history data
+
 ###########################################################
 
 # ------------
