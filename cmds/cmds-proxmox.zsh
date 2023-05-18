c0fb37c566dbd906c978f6d73cbbb7e9f0ad6d0a
Jordan Trask <jtrask@gmail.com>
2023-05-18 00:46:59 -0400

    .

--- cmds/cmds-proxmox.zsh
+++ cmds/cmds-proxmox.zsh
@@ -212,7 +212,7 @@ function proxmox_createtemp () {
     [[ -z ${OS} ]] && OS="focal"
     [[ -z ${BRIDGE} ]] && BRIDGE="vmbr0"
     if [[ -z ${STORAGE} ]]; then
-        STORAGE=$(pvesm status -content images)
+        IFS=$'\n' STORAGE=$(pvesm status -content images| awk {' print $1 '})
     fi
     _debugf "\$OS:$OS \$BRIDGE:$BRIDGE \$STORAGE:$STORAGE \$VM_ID:$VM_ID"
 
