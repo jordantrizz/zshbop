d5627dff53f1e0a696ad4c8ef7568452cf1596bf
Jordan Trask <jtrask@gmail.com>
2023-05-17 22:36:40 -0400

    .

--- cmds/cmds-proxmox.zsh
+++ cmds/cmds-proxmox.zsh
@@ -192,6 +192,20 @@ proxmox_createvm () {
     fi
 }
 
+# -- proxmox_createtemplate
+help_proxmox[proxmox_createtemplate]='Create a template from a VM'
+function proxmox_createtemplate () {
+    curl -o /tmp https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
+    qm create 9000 --memory 2048 --net0 virtio,bridge=vmbr0
+    qm importdisk 9000 /tmp/focal-server-cloudimg-amd64.img local-lvm
+    qm set 9000 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-9000-disk-0
+    qm set 9000 --ide2 local-lvm:cloudinit
+    qm set 9000 --boot c --bootdisk scsi0
+    qm set 9000 --serial0 socket --vga serial0
+    qm template 9000
+}
+
+
 # -- proxmox-info
 help_proxmox[proxmox-info]='Info about Proxmox instance'
 proxmox-info () {
