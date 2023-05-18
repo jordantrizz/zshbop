95884c31d8f9fc93e5c5a828da6e32872082c86e
Jordan Trask <jtrask@gmail.com>
2023-05-18 14:40:05 -0400

    .

--- cmds/cmds-zsh.zsh
+++ cmds/cmds-zsh.zsh
@@ -8,3 +8,9 @@ help_files[zsh]='ZSH related commands'
 
 # - Init help array
 typeset -gA help_zsh
+
+# -- zsh-bin
+help_zsh[zsh-bin]='Install zsh-bin from https://github.com/romkatv/zsh-bin'
+function zsh-bin() {
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh-bin/master/install)"
+}
