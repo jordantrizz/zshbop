89b66691574e5e3026fff5dd3e1269445552bf58
Jordan Trask <jtrask@gmail.com>
2023-05-19 16:52:02 -0400

    .

--- cmds/cmds-system.zsh
+++ cmds/cmds-system.zsh
@@ -72,8 +72,7 @@ function check-cpu-mhz() {
 help_system[system-specs]='Print system specs'
 function system-specs () {
     if [[ $MACHINE_OS == "linux" ]]; then
-        echo " - Sockets: $(lscpu | awk '/^Socket/{print $2}') Cores: $(lscpu | awk '/^Core\(s\) per socket/{print $4}')  Threads: $(lscpu | awk '/^CPU\(s\)/{print $2}')"
-        echo " - System Memory $(free -g | awk '/^Mem:/{print $2}')GB"
+        echo "Sockets: $(lscpu | awk '/^Socket/{print $2}') Cores: $(lscpu | awk '/^Core\(s\) per socket/{print $4}')  Threads: $(lscpu | awk '/^CPU\(s\)/{print $2}') - Memory $(free -g | awk '/^Mem:/{print $2}')GB"
         echo "$(check-cpu-mhz)"	
     else
         _error "system-specs not implemented for $MACHINE_OS"
