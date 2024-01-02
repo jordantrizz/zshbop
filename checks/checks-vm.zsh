127d65fed92f94a29b919ca68a3c48a7bb6b9227
Jordan Trask <jtrask@gmail.com>
2024-01-02 14:11:39 -0500

    improvement(vm-check-detect): Improved vm-check-detect

--- checks/checks-vm.zsh
+++ checks/checks-vm.zsh
@@ -19,39 +19,58 @@ function vm-checks () {
 # -- check if in virtual environment
 # ==============================================
 help_checks[vm-check-detect]='Am I in a VM?'
-function vm-check () {
+function vm-check-detect () {
     _debug "Checking if in virtual environment"
 
     [[ $MACHINE_OS == "mac" ]] && { _success "VM: Running on Mac...no need to check"; return 0 }
 
+    # Initialize VM variable
+    local VM_TYPE="" OUTPUT VIRT_WHAT="0"
+
     # -- check if virt-what exists
     _cexists virt-what
     if [[ $? == "0" ]]; then
-        _debug "virt-what installed"
+        OUTPUT+="virt-what installed - "
         # -- Check if running as root
-        _checkroot
-        if [[ $? == "1" ]] { return 0; }
-        VM=$(virt-what)
-        if [[ -n $VM ]]; then
-            _debug "virt-what returned $VM"
-            if [[ $VM == "kvm" ]]; then
-                if [[ $(pgrep qemu) ]] || [[ $(pgrep qemu-system) ]]; then
-                    echo "$(_loading3 VM-virt-what:) Running on KVM, and qemu guest tools is running"
+        _checkroot >> /dev/null
+        if [[ $? == "1" ]]; then
+            OUTPUT+="not root can't run virt-what - "
+            VIRT_WHAT="0"
+        else
+            VM_TYPE=$(virt-what)
+            if [[ -n $VM ]]; then
+                OUTPUT+="virt-what returned $VM_TYPE - "
+                if [[ $VM_TYPE == "kvm" ]]; then
+                    if [[ $(pgrep qemu) ]] || [[ $(pgrep qemu-system) ]]; then
+                        OUTPUT+="KVM qemu guest tools is running - "
+                    else
+                        OUTPUT+="KVM qemu not running install qemu-guest-agent - "
+                    fi
                 else
-                    _warning "VM-virt-what: Running on KVM, but qemu is not running, install qemu-guest-agent"
+                    OUTPUT+=" - $VM_TYPE"
                 fi
             else
-                _alert "VM-virt-what: Running on $VM" 0
-            fi
-        else
-            _loading3 "Not running in a VM"
-            _debug "virt-what returned $VM"
+                OUTPUT+="Not running in a VM"
+                _debug "virt-what returned $VM_TYPE"
+            fi            
         fi
-    else
-        _warning "Unable to determine if in virtual environment, please install virt-what"
-        
+    fi
+    
+    # -- Fall back to virt-what
+    if [[ $VIRT_WHAT == "0" ]]; then    
+        # Fallback checks
+        if [[ -e /proc/user_beancounters ]] || grep -q -E "(vmware|kvm|xen)" /proc/cpuinfo; then
+            VM=$(grep -E "(vmware|kvm|xen)" /proc/cpuinfo | head -1 | awk '{print $1}')
+            echo "Probably in a virtual environment (fallback check) since /proc/user_beancounters and /proc/cpuinfo = $VM"
+        elif [[ $OS2 == "wsl" ]]; then
+            echo "In WSL, which is a virtual environment (fallback check)."
+            VM_TYPE="wsl"
+        else
+            echo "Not in a virtual environment (fallback check)."
+        fi
     fi
 }
+
 
 # ==============================================
 # -- check if in virtual environment secondary method
