4ae164c5de15a7cd2f97a4b93e154c31e999c17e
Jordan Trask <jtrask@gmail.com>
2024-01-05 15:55:16 -0500

    refactor(check-ssl): Refactored check-ssl

--- cmds/cmds-ssl.zsh
+++ cmds/cmds-ssl.zsh
@@ -11,26 +11,126 @@ typeset -gA help_ssl
 
 # -- ssl-check
 help_ssl[ssl-check]='Check SSL Certificate on host'
-ssl-check () { 
-	if [[ -z $1 ]]; then
-		echo "Usage: ssl-check [-h hostname|-f file]"
-	elif [[ $1 == "-h" ]]; then
-		_loading "-- Checking SSL Certificate on $2"
-		output=$(echo | openssl s_client -showcerts -servername $2 -connect $2:443 2>/dev/null | openssl x509 -inform pem -noout -text)
+ssl-check () {
+	local flag_help flag_verbose arg_filename
+	arg_domain=("BLANK")
+	arg_filename=("BLANK")
+	# -- Check arguments
+	typeset -A opts
+    zparseopts -D -A opts -- \
+        {h,-help}=flag_help \
+        {v,-verbose}=flag_verbose \
+        {f,-filename}:=arg_filename \
+		{d,-domain}:=arg_domain
+    
+	# -- Usage
+	function _ssl_check_usage () {
+		echo ""
+		echo "Usage: ssl-check [-d hostname|-f file]"
+		echo "  -d hostname: Check SSL Certificate on hostname"
+		echo "  -f file: Check SSL Certificate in file"
+		echo "  -v verbose: Verbose output"
+		echo ""
+	}
+
+	# -- _ssl_check_print_summary - args: $1: OUTPUT
+	function _ssl_check_print_summary () {
+		local OUTPUT="$1"
+		# -- Summary
+		_loading " -- Summary"
+		
+		# -- Print Issuer		
+		echo $OUTPUT | grep 'Issuer:'	
+		echo $OUTPUT | grep -A2 'Validity'
+		echo $OUTPUT | grep 'Subject: CN'
+		echo $OUTPUT | grep 'DNS'
+
+	}
+
+	# -- _ssl_check_domain -- args: $1: HOSTNAME, $2: verbose
+	function _ssl_check_domain () {
+		local DOMAIN="$1" VERBOSE="$2" VERBOSE_FLAG="" CHECK_CMD
+		_loading2 "Domain: $DOMAIN Verbose: $VERBOSE"		
+		
+		# -- Check if verbose
+		CHECK_CMD_VERBOSE="echo | openssl s_client -showcerts -servername $DOMAIN -connect $DOMAIN:443 2>/dev/null | openssl x509 -inform pem -noout -text"
+		OUTPUT=$(eval $CHECK_CMD_VERBOSE)				
+		_loading2 "Check command: $CHECK_CMD_VERBOSE"		
+		_loading2 "---------------------------------------------------"
+		
+		if [[ $VERBOSE == "1" ]]; then
+			_loading2 " -- Verbose output"
+			echo $OUTPUT
+		fi
+
+		_ssl_check_print_summary "$OUTPUT"
+		
+	}
+
+	# -- _ssl_check_file -- args: $1: FILENAME, $2: verbose
+	function _ssl_check_file () {
+		local FILENAME="$1" VERBOSE="$2"
+		_loading2 "-- Filename: $FILENAME Verbose: $VERBOSE"
+		_loading2 "---------------------------------------------------"
+		
+		# -- Check if verbose
+		if [[ $VERBOSE == "1" ]]; then
+			_loading2 " -- Verbose output"
+			VERBOSE_FLAG=""
+		else
+			VERBOSE_FLAG="| grep -A2 'Validity' | grep 'Subject: CN' | grep 'DNS'"
+		fi
+		
+		# -- Print output
+		output=$(openssl x509 -in $FILENAME -text $VERBOSE_FLAG)
 		echo $output
 		_loading2 "---------------------------------------------------"
 		echo ""
+		
+		# -- Print validity
 		_loading " -- Grabbing Validity"
 		echo $output | grep -A2 'Validity'
+		
+		# -- Print Subject: CN
 		_loading " -- Grabbing Subject: CN"
 		echo $output | grep 'Subject: CN'
 		echo $output | grep 'DNS'
-	elif [[ $1 == "-f" ]]; then
-		_loading "-- Checking SSL Certificate on $2"
-		openssl x509 -in $2 -text -noout
+	}
+
+	# -- Verbose
+	[[ -n $flag_verbose ]] && VERBOSE="1" || VERBOSE="0"	
+	# -- Check arguments
+	if [[ -n $flag_help ]]; then
+		echo "Printing help"
+		_ssl_check_usage	
+		return 1
+	# -- Domain Check
+	elif [[ -n $arg_domain ]]; then
+		DOMAIN=$arg_domain[2]
+		if [[ $DOMAIN == "BLANK" ]]; then
+			_ssl_check_usage			
+			_error "No domain name passed to -d"
+			return 1
+		else
+			_loading "-- Checking SSL Certificate on domain: $DOMAIN"
+			_ssl_check_domain $DOMAIN $VERBOSE
+		fi
+	# -- File Check
+	elif [[ -n $arg_filename ]]; then
+		FILENAME=$arg_filename[1]
+		if [[ $FILENAME == "BLANK" ]]; then
+			_ssl_check_usage			
+			_error "No filename passed to -f"
+			return 1
+		else
+			_loading "-- Checking SSL Certificate on file: $FILENAME"
+			_ssl_check_file "-f" $FILENAME $flag_verbose
+		fi
 	else 
-		echo "Usage: ssl-check [-h hostname|-f file]"
-	fi
+		_ssl_check_usage
+		_error "Invalid argument passed or no argument passed"
+		return 1
+	fi	
 }
 
 # -- gen-ss-cert
