7f7e9c47cb6f32892394e651e8182ebd536ac14a
Jordan Trask <jtrask@gmail.com>
2024-01-09 21:38:53 -0500

    refactor(cf-check): Major refactor of cf-check

--- cmds/cmds-clouldflare.zsh
+++ cmds/cmds-clouldflare.zsh
@@ -109,91 +109,229 @@ function cf-ip () {
 
 help_cloudflare[cf-check]='Check if a domain is using Cloudflare'
 function cf-check() {
-    local DOMAIN="$1" DIG_OUT WHOIS_OUT SUB_DOMAIN REAL_DOMAIN
+    local CSV_OUTPUT="" 
 
-    if [[ -z $DOMAIN ]]; then
-        _error "Usage: ./cf-check <domain>"
-        return 1
-    fi
+    function _cf_check_usage () {
+        echo "Usage: ./cf-check -c -co -d [<domain>|-f <file>] "
+        echo "  -d  <domain> - Check a single domain"
+        echo "  -f  <file> - Check a file of domains"
+        echo "  -c  - Output CSV"
+        echo "  -co - Output CSV only"
+    }
 
-    # -- Check if domain is proxied through Cloudflare
-    _loading "Checking if $DOMAIN is proxied through Cloudflare"
+    function _cf_check_domain () {
+        local DIG_OUT WHOIS_OUT SUB_DOMAIN REAL_DOMAIN 
+        local DIG_OUT_RESOLVES DIG_OUT_NS DIG_OUT_WWW
+        local DOMAIN="$1"
+                
+        # -- Check if domain is proxied through Cloudflare
+        _loading2 "Checking if $DOMAIN is proxied through Cloudflare"
 
-    # -- Check if domain name or dns name
-    local DOT_COUNT=$(grep -o "\." <<< "$DOMAIN" | wc -l)
-    local DOMAIN_REGEX='^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+([a-zA-Z]{2,63})$'
-    local SUBDOMAIN_REGEX='^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.){2,}([a-zA-Z]{2,63})$'
+        # -- Check if domain name or dns name
+        local DOT_COUNT=$(grep -o "\." <<< "$DOMAIN" | wc -l)
+        local DOMAIN_REGEX='^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+([a-zA-Z]{2,63})$'
+        local SUBDOMAIN_REGEX='^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.){2,}([a-zA-Z]{2,63})$'
 
-    if [[ $DOT_COUNT -ge 2 ]]; then
-        _loading3 "$DOMAIN is a subdomain or DNS record"
-        # Extract the base domain from subdomain
-        REAL_DOMAIN=$(echo $DOMAIN | awk -F'.' '{print $(NF-1)"."$NF}')
-        SUB_DOMAIN=1
-    elif [[ $DOMAIN =~ $DOMAIN_REGEX ]]; then
-        _loading3 "$DOMAIN is a top-level domain"
-        REAL_DOMAIN=$DOMAIN
-    else
-        _loading3 "Invalid format for domain or DNS record - $DOMAIN"
-        return 1
-    fi
-
-    # -- Check cloudflare nameservers via whois
-    _loading3 "Checking if $REAL_DOMAIN uses Cloudflare name servers via whois"
-    WHOIS_OUT=($(whois $REAL_DOMAIN | grep -i "cloudflare.com"))
-    if [[ -n $WHOIS_OUT ]]; then
-        _success "Cloudflare name servers detected - $WHOIS_OUT"
-    else
-        _error "Cloudflare name servers not detected - $WHOIS_OUT"
-    fi
-
-    # Check Cloudflare name servers via dig
-    _loading3 "Checking if $REAL_DOMAIN uses Cloudflare name servers via dig"
-    DIG_OUT=($(dig ns $REAL_DOMAIN +short))
-    if echo $DIG_OUT | grep -iq 'cloudflare'; then
-        _success "Cloudflare name servers detected - $DIG_OUT"
-    else
-        _error "Cloudflare name servers not detected - $DIG_OUT"
-    fi
-
-    # -- Does $DOMAIN resolve?
-    _loading3 "Checking if $DOMAIN resolves"
-    DIG_OUT=($(dig +short $DOMAIN))
-    if [[ -n $DIG_OUT ]]; then
-        _success "$DOMAIN resolves - $DIG_OUT"
-    else
-        _error "$DOMAIN does not resolve - $DIG_OUT"
-        return 1
-    fi
-
-    # Check if apex record is proxied
-    _loading2 "Checking if apex record for $DOMAIN is proxied..."
-    DIG_OUT=($(dig +short $DOMAIN))
-    for IP in "${DIG_OUT[@]}"; do
-        if cf-ip "$IP" > /dev/null 2>&1; then
-            _success "Apex record is proxied through Cloudflare - $IP"
+        if [[ $DOT_COUNT -ge 2 ]]; then        
+            # Extract the base domain from subdomain
+            REAL_DOMAIN=$(echo $DOMAIN | awk -F'.' '{print $(NF-1)"."$NF}')
+            SUB_DOMAIN=1            
+        elif [[ $DOMAIN =~ $DOMAIN_REGEX ]]; then
+            _loading3 "$DOMAIN is a top-level domain"
+            REAL_DOMAIN="$DOMAIN"
+            SUB_DOMAIN=0
         else
-            _error "Apex record is not proxied through Cloudflare - $IP"
+            _loading3 "Invalid format for domain or DNS record - $DOMAIN"
+            return 1
         fi
-    done
 
-    # -- Check if www record is proxied
-    # Check if domain record has www. in the front
-    if [[ $DOMAIN =~ ^www\..* ]]; then
-            _error "Checking www record already"
-    elif [[ $SUB_DOMAIN == "1" ]]; then
-        _loading3 "Not checking www.$DOMAIN record as $DOMAIN is a subdomain"
-    else
-        _loading2 "Checking if www.$DOMAIN record for $DOMAIN is proxied..."
-        DIG_OUT=($(dig +short www.$DOMAIN))
-        for IP in "${DIG_OUT[@]}"; do
+        # -- Add to array
+        cfr_realdomain="$REAL_DOMAIN"
+        cfr_subdomain="$SUB_DOMAIN"            
+
+        # -- Check cloudflare nameservers via whois    
+        WHOIS_OUT=$(whois $REAL_DOMAIN | grep -i "cloudflare.com" | tr '\n' ' ')
+        WHOIS_OUT=${WHOIS_OUT//'Name Server: '/}        
+        cfr_cfnswhois_out="$WHOIS_OUT"
+        if [[ -n $WHOIS_OUT ]]; then
+            _success "Cloudflare name servers detected via WHOIS - $WHOIS_OUT"
+            cfr_cfnswhois="1"
+        else
+            _error "Cloudflare name servers not detected via WHOIS - $WHOIS_OUT"
+            cfr_cfnswhois="0"
+        fi
+
+        # Check Cloudflare name servers via dig    
+        DIG_OUT_NS_TEMP=($(dig ns $REAL_DOMAIN +short))        
+        for dom in "${DIG_OUT_NS_TEMP[@]}"; do
+            TEMP=${dom%\.}            
+            DIG_OUT_NS+="$TEMP "
+        done 
+        cfr_cfnsdig_out="$DIG_OUT_NS"
+        if echo $DIG_OUT_NS | grep -iq 'cloudflare'; then
+            _success "Cloudflare name servers detected via DIG - $DIG_OUT_NS"                                            
+            cfr_cfnsdig=1
+        else
+            _error "Cloudflare name servers not detected via DIG - $DIG_OUT_NS"            
+            cfr_cfnsdig=0
+        fi
+
+        # -- Check if WHOIS and DIG match
+        if [[ $WHOIS_OUT == $DIG_OUT_NS ]]; then
+            _success "WHOIS and DIG match"
+            cfr_cfwhoisdigns_match=1
+        else
+            _error "WHOIS and DIG do not match"
+            cfr_cfwhoisdigns_match=0
+        fi
+
+        # -- Does $DOMAIN resolve?        
+        DIG_OUT_RESOLVES=($(dig +short $DOMAIN))
+        cfr_digresolves_out="$DIG_OUT_RESOLVES"
+        if [[ -n $DIG_OUT_RESOLVES ]]; then
+            _success "$DOMAIN resolves - $DIG_OUT_RESOLVES"
+            cfr_digresolves=1
+        else
+            _error "$DOMAIN does not resolve - $DIG_OUT_RESOLVES"
+            cfr_digresolves=0            
+        fi
+
+        # Check if apex record is proxied    
+        for IP in "${DIG_OUT_RESOLVES[@]}"; do
             if cf-ip "$IP" > /dev/null 2>&1; then
-                _success "www.$DOMAIN record is proxied through Cloudflare - $IP"
+                _success "Apex record is proxied through Cloudflare - $IP"
+                cfr_dnsapex=1
             else
-                _error "www.$DOMAIN record is not proxied through Cloudflare - $IP"
+                _error "Apex record is not proxied through Cloudflare - $IP"
+                cfr_dnsapex=0
             fi
         done
+
+        # -- Check if www record is proxied
+        # Check if domain record has www. in the front
+        if [[ $DOMAIN =~ ^www\..* ]]; then
+                _error "Checking www record already"
+                cfr_wwwresolves="www"
+        elif [[ $SUB_DOMAIN == "1" ]]; then
+            _loading3 "Not checking www.$DOMAIN record as $DOMAIN is a subdomain"
+            cfr_wwwresolves="subdomain"            
+        else            
+            DIG_OUT_WWW=($(dig +short www.$DOMAIN))
+            for IP in "${DIG_OUT_WWW[@]}"; do
+                if cf-ip "$IP" > /dev/null 2>&1; then
+                    _success "www.$DOMAIN record is proxied through Cloudflare - $IP"
+                    cfr_wwwresolves=1
+                    cfr_wwwip="$IP"
+                else
+                    _error "www.$DOMAIN record is not proxied through Cloudflare - $IP"
+                    cfr_wwwresolves=0
+                    cfr_wwwip="$IP"
+                fi
+            done
+        fi
+
+        if [[ -n $CSV ]]; then            
+            _cf_add_to_csv 
+        fi
+    }
+
+    function _cf_check_file () {
+        FILE="$1"
+        DOMAIN=""
+        OUTPUT=""    
+        _loading "Processing file $FILE"   
+
+        if [[ -z $FILE ]]; then
+            _cf_check_usage      
+            _error "No file passed"      
+            return 1
+        fi
+
+        if [[ ! -f $FILE ]]; then
+            _error "File $FILE does not exist"
+            return 1
+        fi
+
+        while read DOMAIN; do          
+            _cf_check_domain $DOMAIN        
+        done < $FILE    
+
+    }
+
+    function _cf_add_to_csv () {
+        local CSV_LINE=""
+        CSV_LINE="$cfr_realdomain,"        
+        CSV_LINE+="$cfr_subdomain,"
+        CSV_LINE+="$cfr_cfnswhois_out,"
+        CSV_LINE+="$cfr_cfnswhois,"
+        CSV_LINE+="$cfr_cfnsdig_out,"
+        CSV_LINE+="$cfr_cfnsdig,"
+        CSV_LINE+="$cfr_cfwhoisdigns_match,"
+        CSV_LINE+="$cfr_digresolves_out,"
+        CSV_LINE+="$cfr_digresolves,"
+        CSV_LINE+="$cfr_dnsapex,"
+        CSV_LINE+="$cfr_wwwresolves,"
+        CSV_LINE+="$cfr_wwwip"            
+        CSV_OUTPUT+="${CSV_LINE}\n" 
+        echo $CSV_OUTPUT       
+    }
+    
+    function _cf_check_output_csv () {             
+        CSV_HEADER="Domain,Real Domain,Subdomain,WHOIS NS,DIG NS,WHOIS and DIG Match,DIG Resolves,DNS Apex,WWW Resolves,WWW IP"
+        CSV_OUTPUT_FINAL="${CSV_HEADER}\n"
+        CSV_OUTPUT_FINAL+="${CSV_OUTPUT}\n"
+        echo "$CSV_OUTPUT_FINAL"
+    }
+
+    local ARG_DOMAIN ARG_FILE CSVOUT CSVONLY DOMAIN FILE HELP
+    zparseopts -D -E c=CSVOUT co=CSVONLY d:=ARG_DOMAIN f:=ARG_FILE h=HELP
+
+    if [[ -n $CSVOUT ]] || [[ -n $CSVONLY ]]; then
+        CSV=1
     fi
 
+    [[ -n $ARG_DOMAIN ]] && DOMAIN=$ARG_DOMAIN[2]
+    [[ -n $ARG_FILE ]] && FILE=$ARG_FILE[2]   
+
+    if [[ -n $HELP ]]; then
+        echo "Printing help"
+        _cf_check_usage        
+        return 0
+    elif [[ -n $DOMAIN ]]; then        
+        _loading "Checking domain $DOMAIN"  
+        if [[ -n $CSVOUT ]]; then
+            _loading2 "Outputting CSV"
+            _cf_check_domain $DOMAIN
+            echo ""            
+            _cf_check_output_csv          
+        elif [[ -n $CSVONLY ]]; then
+            _loading2 "Outputting CSV Only"
+            _cf_check_domain $DOMAIN >> /dev/null
+            echo ""
+            _cf_check_output_csv
+        else
+            _cf_check_domain $DOMAIN            
+        fi        
+    elif [[ -n $FILE ]]; then
+        if [[ -n $CSVOUT ]]; then
+            _loading2 "Outputting CSV"
+            _cf_check_file $FILE
+            echo ""
+            _cf_check_output_csv
+        elif [[ -n $CSVONLY ]]; then
+            _loading2 "Outputting CSV Only"
+            _cf_check_file $FILE >> /dev/null
+            echo ""
+            _cf_check_output_csv
+        else
+            _loading "Checking file $FILE"
+            _cf_check_file $FILE
+        fi        
+    else
+        _error "No arguments"
+        _cf_check_usage
+        return 1
+    fi    
 }
 
 # -- cf-check-file -- Check domains conatined within a file
