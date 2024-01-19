1f07b8431ded09d233cbcc259fa77dd851b26a5a
Jordan Trask <jtrask@gmail.com>
2024-01-19 18:37:42 -0500

    updated

--- cmds/cmds-linux.zsh
+++ cmds/cmds-linux.zsh
@@ -14,7 +14,7 @@ typeset -gA help_linux
 
 # -- swap-find - find what's using swap.
 help_linux[swap-find]='Find what processes are using swap.'
-find-swap () { 
+swap-find () { 
 	find /proc -maxdepth 2 -path "/proc/[0-9]*/status" -readable -exec awk -v FS=":" '{process[$1]=$2;sub(/^[ \t]+/,"",process[$1]);} END {if(process["VmSwap"] && process["VmSwap"] != "0 kB") printf "%10s %-30s %20s\n",process["Pid"],process["Name"],process["VmSwap"]}' '{}' \; | awk '{print $(NF-1),$0}' | sort -h | cut -d " " -f2- 
 }
 
