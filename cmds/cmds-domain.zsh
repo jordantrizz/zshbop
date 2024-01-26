e6a83c9fc2b979a3ed5a03403df22582c1306814
Jordan Trask <jtrask@gmail.com>
2024-01-26 12:51:14 -0500

    improvement(domain): Upadted dom and domain command

--- cmds/cmds-domain.zsh
+++ cmds/cmds-domain.zsh
@@ -22,8 +22,12 @@ help_domain[domain-info]='Check a domains name servers and www and a record and 
 # --- dom
 help_domain[dom]='Check a domains availability, www, mx etc'
 function dom () {
-    domain $1
-    domain-info $1
+    local DOMAIN="$1"
+    domain $1    
+    if [[ $? -eq 0 ]]; then    
+        echo ""
+        domain-info $1
+    fi
 }
 
 # -- domain
@@ -34,7 +38,7 @@ function domain () {
         echo "Check if a domain name is available. Enter in the full domain name"
         echo  "  -a    - check all domain extensions, just enter the first part"    
     }
-    local DOMAIN="$1" CHECK_DOMAIN DOMAIN_EXTS WHOIS_DOMAIN GET_REGISTRAR="0" WHOIS_OUT
+    local DOMAIN DOMAIN_INPUT="$1" CHECK_DOMAIN DOMAIN_EXTS WHOIS_DOMAIN GET_REGISTRAR="0" WHOIS_OUT
 
     # -- No args print usage
     if [[ -z $1 ]]; then
@@ -42,27 +46,32 @@ function domain () {
         return 1
     fi
 
+    domain-strip $DOMAIN_INPUT >> /dev/null
+    DOMAIN=$OUTPUT_DOMAIN_STRIP
+
     # -- Check all domain extensions
     _loading "Checking $DOMAIN"
     if [[ $1 == "-a" ]]; then
-        echo " - Checking all domain extensions"
+        _loading3  "Checking all domain extensions"
         CHECK_DOMAIN="2"
         DOMAIN_EXTS=('.com' '.co.uk' '.net' '.info' '.mobi')
         DOMAIN_EXTS+=('.org' '.tel' '.biz' '.tv' '.cc' '.eu' '.ru')
         DOMAIN_EXTS+=('.in' '.it' '.sk' '.com.au')
     else
+        _loading3 "Checking single domain $DOMAIN"
         CHECK_DOMAIN="1"
     fi
 
     # -- Check if domain is available
     WHOIS_OUT=$(whois $DOMAIN)
     if [[ $CHECK_DOMAIN == "1" ]]; then
-        echo "Checking single domain $DOMAIN"
+        
         WHOIS_DOMAIN=$(echo $WHOIS_OUT | egrep -q '^No match|^NOT FOUND|^Not fo|AVAILABLE|^No Data Fou|has not been regi|No entri')        
         if [[ $? -eq 0 ]]; then
-            echo -e "${DOMAIN}: $bg[green] > Available to Register  < ${RSC}"            
+            echo -e "Registration Status $bg[green] > ${DOMAIN}: Available to Register < ${RSC}"
+            return 1
         else 
-            echo "Registration Status : $bg[red]X Registered X ${RSC}"
+            echo "Registration Status : $bg[red]X ${DOMAIN}: Registered X ${RSC}"
             GET_REGISTRAR=1
         fi        
     elif [[ $CHECK_DOMAIN == "2" ]]; then
@@ -103,4 +112,33 @@ function domain () {
 
     fi
     echo ""
+}
+
+# ----------------------------------------------------------------------------------------------------
+# -- domain-strip $domain $quiet
+# --
+# -- Strip a domain name to the base domain
+# -- Example: domain-strip "https://www.google.com" -> google.com
+# -- Echos and Sets: OUTPUT_DOMAIN_STRIP
+# ----------------------------------------------------------------------------------------------------
+help_domain[domain-strip]='Strip a domain name to the base domain'
+function domain-strip () {
+    local QUIET=${2:="0"} DOMAIN_INPUT="$1"
+    
+    _loading "Checking $DOMAIN_INPUT and stripping any extra characters"
+    # -- Check if https:// is in the domain name
+
+    [[ $DOMAIN_INPUT == *"https://"* ]] && { DOMAIN_INPUT=${DOMAIN_INPUT#"https://"}; [[ $QUIET=="0" ]] && _loading3 "Stripped https://"; }
+    # -- Check if http:// is in the domain name
+    [[ $DOMAIN_INPUT == *"http://"* ]] && { DOMAIN_INPUT=${DOMAIN_INPUT#"http://"}; _loading3 "Stripped http://"; }
+    # -- Check if www is in the domain name
+    [[ $DOMAIN_INPUT == *"www."* ]] && { DOMAIN_INPUT=${DOMAIN_INPUT#"www."}; _loading3 "Stripped www"; }
+    # -- Remove / with anything else past /
+    [[ $DOMAIN_INPUT == *"/"* ]] && { DOMAIN_INPUT=${DOMAIN_INPUT%%/*}; _loading3 "Stripped /"; }
+    # -- Remove anything after a space
+    [[ $DOMAIN_INPUT == *" "* ]] && { DOMAIN_INPUT=${DOMAIN_INPUT%% *}; _loading3 "Stripped space"; }    
+    
+    # -- echo and set
+    echo $DOMAIN_INPUT
+    OUTPUT_DOMAIN_STRIP="$DOMAIN_INPUT"
 }
