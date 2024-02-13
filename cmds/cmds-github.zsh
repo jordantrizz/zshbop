d29fdda36e799bd9b676185e7e41e2109f5157b4
Jordan Trask <jtrask@gmail.com>
2024-02-13 09:16:48 -0500

    feat(github): Added gh-web function as short for gh repo view --web

--- cmds/cmds-github.zsh
+++ cmds/cmds-github.zsh
@@ -1,22 +1,23 @@
-# --
-# replace commands
-#
-# Example help: help_template[test]='Generate phpinfo() file'
-#
-# --
+# -- github-cli commands
 _debug " -- Loading ${(%):-%N}"
-
-# What help file is this?
 help_files[github]="Github related commands"
-
-# - Init help array
 typeset -gA help_github
-
 _debug " -- Loading ${(%):-%N}"
 
-# -- paths
+# ==================================================
+# gh-start
+# ==================================================
 help_github[gh-start]='An extension for gh-cli for starting a PR'
 function gh-start () {
 	echo "Installing gh-start extension from gh extension install https://github.com/balvig/gh-start"
 	gh extension install https://github.com/balvig/gh-start
 }
+
+# ==================================================
+# gh-web
+# ==================================================
+help_github[gh-web]='Open the current repository in the browser'
+function gh-web () {
+	gh repo view --web
+}
+
