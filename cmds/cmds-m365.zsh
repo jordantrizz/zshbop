7b41a19047114c60f4768e6928a3163b608ca3ef
Jordan Trask <jtrask@gmail.com>
2024-09-23 19:15:56 -0400

    feat(powershell): Added powershell commands.

---
+++ cmds/cmds-m365.zsh
@@ -0,0 +1,22 @@
+# --
+# powershell commands
+# --
+_debug " -- Loading ${(%):-%N}"
+
+# What help file is this?
+help_[powershell]="Powershell commands"
+
+# - Init help array
+typeset -gA help_powershell
+
+_debug " -- Loading ${(%):-%N}"
+
+# ===============================================
+# -- m365-add-email
+# ===============================================
+help_powershell[m365-add-email]='Add an email to a Microsoft 365 account.'
+m365-add-email () {
+    m365_add_email_usage () {
+        echo "Usage: m365-add-email [-e <email>|-d <domain>] [-u <username>|-all]"
+    }
+}
