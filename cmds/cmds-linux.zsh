b266f73abdeb2fced9f1253e320bf323ab0fc3a6
Jordan Trask <jtrask@gmail.com>
2024-09-25 12:10:26 -0400

    Small fix

--- cmds/cmds-linux.zsh
+++ cmds/cmds-linux.zsh
@@ -781,7 +781,7 @@ smartctl-all-disks () {
 		for i in $DEVICES; do
 			_loading2 "Disk $i"
 			SMARTCTL_OUTPUT=$(smartctl -i -A /dev/$i)
-			echo $SMARTCTL_OUPUT | egrep -E -i "SERIAL|Device Model|Firmware Version|User Capacity|TRIM Command|SMART support is"
+			echo $SMARTCTL_OUTPUT | egrep -E -i "Serial Number|Device Model|Firmware Version|User Capacity|TRIM Command|SMART support is"
 			echo $SMARTCTL_OUTPUT | egrep -E -i "^  "5"|^"197"|^"198"|"FAILING_NOW""			
 		done
 	elif [[ -n $ERRORS_ONLY ]]; then
