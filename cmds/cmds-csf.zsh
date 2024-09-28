fe9776eb31047dfbb6f6eb720890751104760890
Jordan Trask <jtrask@gmail.com>
2024-09-28 06:39:33 -0400

    feat(csf): Created cmds-csf.zsh

---
+++ cmds/cmds-csf.zsh
@@ -0,0 +1,17 @@
+# =================================================================================================
+# csf commands
+# =================================================================================================
+# Example help: help_template[test]='Generate phpinfo() file'
+_debug " -- Loading ${(%):-%N}"
+help_files[csf]="CSF Commands and scripts"
+typeset -gA help_csf
+_debug " -- Loading ${(%):-%N}"
+
+
+# ===============================================
+# -- lfd-restart
+# ===============================================
+help_csf[lfd-restart]="Restart the lfd service"
+lfd-restart () {
+    sudo csf --lfd restart
+}
