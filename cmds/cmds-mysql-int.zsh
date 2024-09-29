2ffd10688700c68d14b00d972e3d7d0a83d15571
Jordan Trask <jtrask@gmail.com>
2024-09-28 21:05:42 -0400

    Fix

--- cmds/cmds-mysql-int.zsh
+++ cmds/cmds-mysql-int.zsh
@@ -13,6 +13,7 @@ MYSQL_PAGER=
 # ===============================================
 help_int_mysql[_zb_mysql_wrapper]='Set mysql wrapper'
 function   () {
+	_log "Setting mysql wrapper"
 	# -- Check if mysql_wrapper is installed
 	_cmd_exists _mysql_wrapper
 	if [[ $? == 1 ]]; then
