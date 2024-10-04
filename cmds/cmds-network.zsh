6f63e75db465b656a53f695dc9e04978746e37b2
Jordan Trask <jtrask@gmail.com>
2024-10-04 18:39:10 -0400

    feat(network): Created ports command

--- cmds/cmds-network.zsh
+++ cmds/cmds-network.zsh
@@ -48,3 +48,42 @@ function network-ports () {
 	_loading "Network Ports via /proc/net/tcp"
 	awk '$4 == "0A" { port=substr($2, index($2, ":")+1); print "Port:", strtonum("0x" port) }' /proc/net/tcp /proc/net/tcp6 2>/dev/null || awk '$4 == "0A" { port=substr($2, index($2, ":")+1); cmd="echo $((0x" port "))"; cmd | getline port; close(cmd); print "Port:", port }' /proc/net/tcp /proc/net/tcp6
 }
+
+# ===============================================
+# -- ports
+# ===============================================
+help_network[ports]="List commonly used ports"
+function ports () {
+	_loading "Commonly Used Ports"
+	typeset -A zb_ports
+	zb_ports[21]="FTP"
+	zb_ports[22]="SSH"
+	zb_ports[23]="Telnet"
+	zb_ports[25]="SMTP"
+	zb_ports[53]="DNS"
+	zb_ports[80]="HTTP"
+	zb_ports[110]="POP3"
+	zb_ports[143]="IMAP"
+	zb_ports[443]="HTTPS"
+	zb_ports[465]="SMTPS"
+	zb_ports[587]="SMTP"
+	zb_ports[993]="IMAPS"
+	zb_ports[995]="POP3S"
+	zb_ports[3306]="MySQL"
+	zb_ports[5432]="PostgreSQL"
+	# Proxmox
+	zb_ports[8006]="Proxmox"
+	# Cyberpanel
+	zb_ports[8080]="HTTP"
+	zb_ports[8090]="Cyberpanel"
+	zb_ports[8443]="HTTPS"
+	zb_ports[10000]="Webmin"
+	zb_ports[10050]="Zabbix"
+	zb_ports[11211]="Memcached"
+
+	# Sort $zb_ports by key
+	for key in ${(kn)zb_ports}; do
+		echo "Port: $key - ${zb_ports[$key]}"
+	done
+
+}
