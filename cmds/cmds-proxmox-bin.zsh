9ff86f00232a311e5813b144988de16430879aca
Jordan Trask <jtrask@gmail.com>
2024-10-22 19:41:08 -0400

    Small Fixes

--- cmds/cmds-proxmox-bin.zsh
+++ cmds/cmds-proxmox-bin.zsh
@@ -597,11 +597,10 @@ function _proxmox_createvm () {
         
         # Get mac address
         # net0: virtio=BC:24:11:7F:72:04,bridge=vmrb0
-        VM_MAC=$(qm config $VM_ID | grep 'net0' | awk '{ print $2 }' | awk -F',' '{ print $1 }' | awk -F'=' '{ print $2 }')
+        VM_MAC_UPPER=$(qm config $VM_ID | grep 'net0' | awk '{ print $2 }' | awk -F',' '{ print $1 }' | awk -F'=' '{ print $2 }')
         # Convert VM_MAC to lower case
-        VM_MAC=${VM_MAC:l}
-        
-
+        VM_MAC="${VM_MAC_UPPER:l}"
+        _loading3 "MAC address is $VM_MAC_UPPER changing to lowercase $VM_MAC"
 
         _loading3 "Setting IP and GW on ipconfig0 with $IP and $GW and $ADDITIONAL_IP and $VM_MAC"
         _proxmox_generate_ip ipconfig0
