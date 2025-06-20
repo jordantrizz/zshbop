ec710cd08f3c8f934d3129ba8fc5d6a0cd9ac2eb
Jordan Trask <jtrask@gmail.com>
2025-06-20 08:16:08 -0400

    Small fixes

--- cmds/cmds-proxmox.zsh
+++ cmds/cmds-proxmox.zsh
@@ -29,35 +29,50 @@ function proxmox-backup.sh () {
 help_proxmox[proxmox-memory-report]='Get memory report for Proxmox'
 function proxmox-memory-report () {
     _loading "Running proxmox-memory-report"
-    # Get a list of all vms
+    # Get a list of all VMs
     local VMS=($(qm list | awk 'NR>1 {print $1}'))
     local TOTAL_MEMORY=0
     local TOTAL_USED=0
     local TOTAL_FREE=0
 
-    # Loop through each vm and get memory usage
+    # Initialize output
     local OUTPUT="VM\tName\tTotal Memory\tUsed Memory\tFree Memory\n"
     OUTPUT+="--\t----\t------------\t-----------\t----------\n"
+
     for VM in $VMS; do
         local NAME=$(qm config $VM | grep name | awk '{print $2}')
         local MEMORY=$(qm config $VM | grep memory | awk '{print $2}')
-        # Used
+        MEMORY=${MEMORY:-0} # Default to 0 if MEMORY is empty
+
+        # Initialize used and free memory
+        local USED=0
+        local FREE=$MEMORY
+
+        # Get balloon memory info
         local BALLOON=$(qm-monitor $VM info balloon | grep balloon:)
         if [[ -n "$BALLOON" ]]; then
             # Extract total_mem and free_mem (both in MB)
             local TOTAL_MEM=$(echo $BALLOON | sed -n 's/.*total_mem=\([0-9]*\).*/\1/p')
             local FREE_MEM=$(echo $BALLOON | sed -n 's/.*free_mem=\([0-9]*\).*/\1/p')
+            TOTAL_MEM=${TOTAL_MEM:-0} # Default to 0 if TOTAL_MEM is empty
+            FREE_MEM=${FREE_MEM:-0}   # Default to 0 if FREE_MEM is empty
+
             if [[ -n "$TOTAL_MEM" && -n "$FREE_MEM" ]]; then
                 USED=$((TOTAL_MEM - FREE_MEM))
                 FREE=$FREE_MEM
             fi
         fi
-        local FREE=$((MEMORY - USED))
+
+        # Update totals
         TOTAL_MEMORY=$((TOTAL_MEMORY + MEMORY))
         TOTAL_USED=$((TOTAL_USED + USED))
         TOTAL_FREE=$((TOTAL_FREE + FREE))
+
+        # Append to output
         OUTPUT+="$VM\t$NAME\t$MEMORY\t$USED\t$FREE\n"
     done
+
+    # Print the report
     echo "$OUTPUT" | column -t -s $'\t'
     echo "-----------------------------------------------------"
     echo "Total VM Memory: $TOTAL_MEMORY"
@@ -65,7 +80,7 @@ function proxmox-memory-report () {
     echo "Total SYS Memory: $TOTAL_SYS_MEMORY"
     echo "% Available: $((100 * (TOTAL_SYS_MEMORY - TOTAL_MEMORY) / TOTAL_SYS_MEMORY))%"
     echo "-----------------------------------------------------"
-    echo "Total Used: N/A"
-    echo "Total Free: N/A"
+    echo "Total Used: $TOTAL_USED"
+    echo "Total Free: $TOTAL_FREE"
     _success "Proxmox memory report generated successfully."
 }
