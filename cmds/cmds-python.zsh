ea7eab630f03730951728db732c1df49176b59e8
Jordan Trask <jtrask@gmail.com>
2025-09-20 08:46:37 -0400

    Added python-venv

--- cmds/cmds-python.zsh
+++ cmds/cmds-python.zsh
@@ -1,14 +1,26 @@
+# =====================================
 # -- python commands
+# =====================================
 _debug " -- Loading ${(%):-%N}"
 typeset -gA help_python
 help_files[python]="Python commands"
 
 
-# --------------------------------------------------
+# =====================================
 # -- python-clean
-# --------------------------------------------------
+# =====================================
 help_python[python-clean]='Clean python cache files'
 function python-clean () {
-        find . -type f -name "*.py[co]" -delete
-        find . -type d -name "__pycache__" -delete
+    find . -type f -name "*.py[co]" -delete
+    find . -type d -name "__pycache__" -delete
 }
+
+# =====================================
+# -- python-venv
+# =====================================
+help_python[python-venv]='Create a python virtual environment in the current directory'
+function python-venv () {
+    python3 -m venv venv
+    source venv/bin/activate
+}
+
