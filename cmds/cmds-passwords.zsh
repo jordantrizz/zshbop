75b9e8ea3818ddc140f55070203f974e5934fab3
Jordan Trask <jtrask@gmail.com>
2026-08-28 15:31:24 -0400

    feat: port genpass apple/monkey/xkcd generators natively

--- cmds/cmds-passwords.zsh
+++ cmds/cmds-passwords.zsh
@@ -114,6 +114,146 @@ function genpass-special () {
 }
 
 # ==================================================
+# -- genpass-apple - pronounceable pseudoword passphrase (cvccvc)
+# ==================================================
+help_passwords[genpass-apple]='Generate pronounceable pseudoword passphrase'
+function genpass-apple () {
+    local -a opts_help
+    zparseopts -D -E -- h=opts_help -help=opts_help
+    if [[ -n $opts_help ]]; then
+        echo "Usage: genpass-apple [count]"
+        return 0
+    fi
+
+    local count=${1:-1}
+    if [[ $count != <1-1000> ]]; then
+        _genpass_error "genpass-apple: invalid count: $count (must be 1-1000)"
+        return 1
+    fi
+
+    local -r vowels=aeiouy
+    local -r consonants=bcdfghjklmnpqrstvwxz
+    local -r digits=0123456789
+    local REPLY chars c pwd digit
+    local -a words
+    local i j k
+    for (( i = 0; i < count; i++ )); do
+        words=()
+        for (( j = 0; j < 6; j++ )); do
+            words+=('')
+            for (( k = 0; k < 2; k++ )); do
+                for chars in $consonants $vowels $consonants; do
+                    _genpass_rand $#chars || return 1
+                    words[-1]+=$chars[REPLY]
+                done
+            done
+        done
+        pwd=${(j:-:)words}
+        # Replace either the first or the last character in one of the words with a digit
+        _genpass_rand $#digits || return 1
+        digit=$digits[REPLY]
+        _genpass_rand $((2 * $#words)) || return 1
+        pwd[REPLY/2*7+2*(REPLY%2)-1]=$digit
+        # Convert one lower-case character to upper case (locale-safe via octal printf)
+        while true; do
+            _genpass_rand $#pwd || return 1
+            [[ $vowels$consonants == *$pwd[REPLY]* ]] && break
+        done
+        c=$pwd[REPLY]
+        printf -v c '%o' $((#c - 32))
+        printf "%s\\$c%s" "$pwd[1,REPLY-1]" "$pwd[REPLY+1,-1]"
+        print -r -- " $(_genpass_entropy 5760000 6)"
+    done
+}
+
+# ==================================================
+# -- genpass-monkey - unambiguous base32-style password (26 chars)
+# ==================================================
+help_passwords[genpass-monkey]='Generate unambiguous base32-style password (26 chars)'
+function genpass-monkey () {
+    local -a opts_help
+    zparseopts -D -E -- h=opts_help -help=opts_help
+    if [[ -n $opts_help ]]; then
+        echo "Usage: genpass-monkey [count]"
+        return 0
+    fi
+
+    local count=${1:-1}
+    if [[ $count != <1-1000> ]]; then
+        _genpass_error "genpass-monkey: invalid count: $count (must be 1-1000)"
+        return 1
+    fi
+
+    local -r chars=abcdefghjkmnpqrstvwxyz0123456789
+    local REPLY i j
+    for (( i = 0; i < count; i++ )); do
+        for (( j = 0; j < 26; j++ )); do
+            _genpass_rand $#chars || return 1
+            print -rn -- $chars[REPLY]
+        done
+        print -r -- " $(_genpass_entropy $#chars 26)"
+    done
+}
+
+# ==================================================
+# -- genpass-xkcd - word passphrase from /usr/share/dict/words
+# ==================================================
+help_passwords[genpass-xkcd]='Generate word passphrase (xkcd style)'
+function genpass-xkcd () {
+    local -a opts_help
+    zparseopts -D -E -- h=opts_help -help=opts_help
+    if [[ -n $opts_help ]]; then
+        echo "Usage: genpass-xkcd [count]"
+        return 0
+    fi
+
+    local count=${1:-1}
+    if [[ $count != <1-1000> ]]; then
+        _genpass_error "genpass-xkcd: invalid count: $count (must be 1-1000)"
+        return 1
+    fi
+
+    local dict=${GENPASS_DICT:-/usr/share/dict/words}
+    if [[ ! -e $dict ]]; then
+        for dict in /usr/share/dict/words /usr/share/dict/american-english /usr/share/dict/linux.words; do
+            [[ -e $dict ]] && break
+        done
+    fi
+    if [[ ! -e $dict ]]; then
+        _genpass_error "genpass-xkcd: no word list found (tried \$GENPASS_DICT, /usr/share/dict/words, /usr/share/dict/american-english, /usr/share/dict/linux.words)"
+        return 1
+    fi
+
+    setopt local_options extended_glob
+    local -a words
+    words=(${(M)${(f)"$(<$dict)"}:#[a-zA-Z](#c1,6)}) || return 1
+    if (( $#words < 2 )); then
+        _genpass_error "genpass-xkcd: not enough suitable words in $dict"
+        return 1
+    fi
+
+    # Each word adds log2($#words) bits; need at least 128 bits of security margin
+    local -i n=$((ceil(128. / (log($#words) / log(2)))))
+    local c rnd i j k
+    for (( i = 0; i < count; i++ )); do
+        print -rn -- $n
+        for (( j = 0; j < n; j++ )); do
+            while true; do
+                # Generate a random number in [0, 2**31) and avoid bias
+                rnd=0
+                for (( k = 0; k < 4; k++ )); do
+                    sysread -s1 c < /dev/urandom || return 1
+                    (( rnd = (~(1 << 23) & rnd) << 8 | #c ))
+                done
+                (( rnd < 16#7FFFFFFF / $#words * $#words )) && break
+            done
+            print -rn -- -$words[rnd%$#words+1]
+        done
+        print -r -- " $(_genpass_entropy $#words $n)"
+    done
+}
+
+# ==================================================
 # -- genpass - generate passwords of multiple types
 # ==================================================
 help_passwords[genpass]='Generate passwords (alnum|special|apple|monkey|xkcd)'
